cfc2e4069a3ca24227117c23d3211d69efe70a02
mountz2641 <tharit2641@hotmail.com>
2017-12-17 03:02:18 +0700

    adjust screen size

--- core/src/com/phu/freefall/game/FreeFall.java
+++ core/src/com/phu/freefall/game/FreeFall.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FreeFall extends Game {
 	SpriteBatch batch;
-	public static final int HEIGHT = 950;
+	public static final int HEIGHT = 1000;
 	public static final int WIDTH = 576;
 	
 	@Override
